a81270b3b749f018bd5925f2f5f15d9296e5c491
Dmitriy <dmitrytrotss@gmail.com>
2023-03-09 17:11:05 +0500

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Dmitriy Trotsenko");
+        System.out.println("24 august 1998");
+    }
+}
