83429708c2ead60d8a491392e1936279132df3c8
Dmitriy <dmitrytrotss@gmail.com>
2023-06-09 20:05:43 +0500

    6.7.3. Массив из диагонали матрицы. [#214128]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -23,16 +23,32 @@ public class MatrixCheck {
         return result;
     }
 
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int index = 0; index < board.length; index++) {
+            rsl[index] = board[index][index];
+        }
+        return rsl;
+    }
+
     public static void main(String[] args) {
-        System.out.println("По горизонтали: " + monoHorizontal(new char[][] {
-                {' ', ' ', ' '},
-                {'X', 'X', 'X'},
-                {'X', 'X', ' ', 'X'}},
+        System.out.println("По горизонтали: " + monoHorizontal(new char[][]{
+                        {' ', ' ', ' '},
+                        {'X', 'X', 'X'},
+                        {'X', 'X', ' ', 'X'}},
                 0));
-        System.out.println("По вертикали: " + monoVertical(new char[][] {
+        System.out.println("По вертикали: " + monoVertical(new char[][]{
                         {'X', ' ', ' '},
                         {'X', 'X', 'X'},
                         {'X', 'X', ' ', 'X'}},
                 0));
+        System.out.println("Символы из диагонали: ");
+        for (char symbol : extractDiagonal(new char[][]{
+                {'A', ' ', ' '},
+                {' ', 'A', ' '},
+                {' ', ' ', 'A'},
+        })) {
+            System.out.println(symbol);
         }
+    }
 }
