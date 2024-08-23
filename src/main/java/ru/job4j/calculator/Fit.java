3c9ed67053eb343f492b27de0073704ddb7e06c6
Dmitriy <dmitrytrotss@gmail.com>
2024-08-23 16:44:06 +0500

    GPT. Нейросети спешат на помощь. [#505156 #509915]

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -3,20 +3,32 @@ package ru.job4j.calculator;
 public class Fit {
 
     public static double manWeight(short height) {
-        double rsl = (height - 100) * 1.15;
-        return rsl;
+        return (height - 100) * 1.15;
     }
 
     public static double womanWeight(short height) {
-        double rsl = (height - 110) * 1.15;
-        return rsl;
+        return (height - 110) * 1.15;
     }
 
     public static void main(String[] args) {
-        short height = 160;
-        double man = manWeight(height);
-        double woman = womanWeight(height);
-        System.out.println("Man 175cm is " + man);
-        System.out.println("Woman 175cm is " + woman);
+        args[0] = "123";
+        if (args.length > 0) {
+            try {
+                short height = Short.parseShort(args[0]);
+                if (height > 0 && height < 300) {
+                    double man = manWeight(height);
+                    double woman = womanWeight(height);
+                    System.out.println("Man " + height + "cm is " + man + " kg");
+                    System.out.println("Woman " + height + "cm is " + woman + " kg");
+                } else {
+                    System.out.println("Please enter a valid height between 1 and 300 cm.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number for height.");
+            }
+        } else {
+            System.out.println("Please provide height as an argument.");
+        }
     }
 }
+
--- src/test/java/ru/job4j/calculator/FitTest.java
+++ src/test/java/ru/job4j/calculator/FitTest.java
@@ -2,6 +2,7 @@ package ru.job4j.calculator;
 
 import org.junit.Assert;
 import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FitTest {
 
@@ -20,4 +21,37 @@ public class FitTest {
         double out = Fit.womanWeight(in);
         Assert.assertEquals(expected, out, 0.01);
     }
+
+    @Test
+    public void whenManHeight180ThenWeight92() {
+        short height = 180;
+        double expected = 92.0;
+        double out = Fit.manWeight(height);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenManHeight150ThenWeight57dot5() {
+        short height = 150;
+        double expected = 57.5;
+        double out = Fit.manWeight(height);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWomanHeight180ThenWeight80dot5() {
+        short height = 180;
+        double expected = 80.5;
+        double out = Fit.womanWeight(height);
+        assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    public void whenWomanHeight150ThenWeight46() {
+        short height = 150;
+        double expected = 46.0;
+        double out = Fit.womanWeight(height);
+        assertEquals(expected, out, 0.01);
+    }
 }
+
